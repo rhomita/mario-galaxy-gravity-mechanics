7fdbb3c012757abbfc75c5eed199c1407371d2d8
Rhomita <roman.glom@gmail.com>
2021-03-30 22:14:46 +0100

    Added rotation to the gravity body

--- Assets/Scripts/GravityBody.cs
+++ Assets/Scripts/GravityBody.cs
@@ -14,11 +14,14 @@ public class GravityBody : MonoBehaviour
     void FixedUpdate()
     {
         _rigidbody.AddForce(GravityDirection * (200 * Time.fixedDeltaTime), ForceMode.Acceleration);
+
+        Quaternion upRotation = Quaternion.FromToRotation(transform.up, -GravityDirection);
+        Quaternion newRotation = Quaternion.Slerp(_rigidbody.rotation, upRotation * _rigidbody.rotation, Time.fixedDeltaTime * 3f);;
+        _rigidbody.MoveRotation(newRotation);
     }
 
     public void SetGravityDirection(Vector3 direction)
     {
-        Debug.Log($"Setting gravity of {gameObject.name} to {direction}");
         direction = direction.normalized; // It is probably normalized, we do it again just in case.
         GravityDirection = direction;
     }
